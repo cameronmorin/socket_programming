6d0b98dd450ee3f7d75181129ae497915bd91532
Cameron <morincam03@gmail.com>
2018-10-22 11:34:11 -0700

    Add message sending capabilities

--- client.py
+++ client.py
@@ -11,6 +11,7 @@ except socket.error, msg:
 print 'Socket Created'
 
 host = 'www.morincameron.com'
+port = 6035
 
 try:
   remote_ip = socket.gethostbyname( host )
@@ -20,3 +21,19 @@ except socket.gaierror:
   sys.exit()
 
 print 'IP address of ' + host + ' is ' + remote_ip
+
+#Connect to the server
+s.connect((remote_ip, port))
+
+print 'Socket Connected to ' + host + ' on ip ' + remote_ip
+
+#Send some data to remote server
+message = "GET / HTTP/1.1\r\n\r\n"
+
+try:
+  s.sendall(message)
+except socket.error:
+  print 'Send Failed'
+  sys.exit()
+
+print 'Message send successfully'
